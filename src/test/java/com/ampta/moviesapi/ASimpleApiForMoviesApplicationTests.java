d074677321be46945ec609dd12590a40997e9cf5
shivam gupta <timenakanu@gmail.com>
2024-04-15 14:15:16 +0530

    first commit

---
+++ src/test/java/com/ampta/moviesapi/ASimpleApiForMoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ampta.moviesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ASimpleApiForMoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
